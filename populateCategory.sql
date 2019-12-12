bd14c236564147c936b0e95d27007d8a993d2df3
Temiloluwa <tehmi2000@gmail.com>
2019-12-12 22:07:31 +0100

    Minor updates to the categories page ui

--- populateCategory.sql
+++ populateCategory.sql
@@ -4,5 +4,9 @@ INSERT INTO categories (title, image) VALUES ('Electronics', 'icofont-jacket');
 INSERT INTO categories (title, image) VALUES ('Beauty & Health Care', 'icofont-girl-alt');
 INSERT INTO categories (title, image) VALUES ('Phones & Tablet', 'icofont-windows-lumia');
 INSERT INTO categories (title, image) VALUES ('Baby, Kids & Toys', 'icofont-baby-trolley');
+INSERT INTO categories (title, image) VALUES ('Groceries', 'icofont-broccoli');
+INSERT INTO categories (title, image) VALUES ('Food Items And Drinks', 'icofont-fast-food');
 INSERT INTO categories (title, image) VALUES ('Special Offer & Deals', 'icofont-gift');
 INSERT INTO categories (title, image) VALUES ('Others', 'icofont-light-bulb');
+
+-- DELETE FROM `categories` WHERE `categories`.`id` = 8
