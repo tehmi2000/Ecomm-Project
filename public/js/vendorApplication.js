e25c703187b25d1cbc66cd9ec4d28f4a5e406720
Temiloluwa <tehmi2000@gmail.com>
2019-12-14 06:43:20 +0100

    Added the js handler for the form and navigation

--- public/js/vendorApplication.js
+++ public/js/vendorApplication.js
@@ -1,5 +1,57 @@
-document.addEventListener("DOMContentLoaded", function(){
+const addHandlers = function() {
+    const container = document.querySelector("#vendor-form .container");
 
+    const navOne = document.querySelector(`.form-navigation span:nth-child(1)`);
+    const navTwo = document.querySelector(`.form-navigation span:nth-child(2)`);
+    const navThree = document.querySelector(`.form-navigation span:nth-child(3)`);
+
+    const nextBtnOne = document.querySelector("#vendor-form .step:first-child .btn");
+    const nextBtnTwo = document.querySelector("#vendor-form .step:nth-child(2) .btn");
+    const submitBtn = document.querySelector("#vendor-form .step:last-child .btn");
+
+    const setActive = function(elementNumber) {
+        // document.querySelectorAll(".form-navigation::after")
+        document.querySelectorAll(".form-navigation span.on").forEach(element => {
+            element.classList.remove("on");
+        });
+
+        for (let i = 1; i <= elementNumber; i++){
+            document.querySelector(".form-navigation .progress").style.width = `${50 * (i-1)}%`;
+            document.querySelector(`.form-navigation span:nth-child(${i})`).classList.add("on");
+            document.querySelector(`.form-navigation span:nth-child(${i})`).removeAttribute("disabled");
+        }
+    };
+
+    const stepOne = function(evt) {
+        if(evt.currentTarget.getAttribute("disabled") === null){
+            setActive(1);
+            container.style.marginLeft = "0%";
+        }
+    };
+
+    const stepTwo = function(evt) {
+        if(evt.currentTarget.getAttribute("disabled") === null){
+            setActive(2);
+            container.style.marginLeft = "-100%";
+        }
+    };
+
+    const stepThree = function(evt) {
+        if(evt.currentTarget.getAttribute("disabled") === null){
+            setActive(3);
+            container.style.marginLeft = "-200%";
+        }
+    };
+
+    navOne.addEventListener("click", stepOne);
+    navTwo.addEventListener("click", stepTwo);
+    nextBtnOne.addEventListener("click", stepTwo);
+    navThree.addEventListener("click", stepThree);
+    nextBtnTwo.addEventListener("click", stepThree);
+};
+
+document.addEventListener("DOMContentLoaded", function(){
+    addHandlers();
 });
 
 const formHandler = (evt) => {
