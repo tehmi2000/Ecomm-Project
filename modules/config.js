caa7cb5485be61ae470cc82f4e503728908b4072
Temiloluwa <tehmi2000@gmail.com>
2019-12-15 03:38:20 +0100

    Vendor application Page UI update and config for vendor finalized

--- modules/config.js
+++ modules/config.js
@@ -100,28 +100,25 @@ const model = function(){
 			log(error);
 		}
 	}
-	
+
 	const userTableExist = () => {
 		const queryCreate = "CREATE TABLE users (id INT(100) NOT NULL AUTO_INCREMENT PRIMARY KEY, uID VARCHAR(100) NOT NULL, username VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, telcode VARCHAR(255) NOT NULL, phone VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, address VARCHAR(255) NOT NULL, profile_picture VARCHAR(255) NOT NULL, verification_status BOOLEAN) ENGINE=InnoDB  DEFAULT CHARSET=utf8";
 		const queryTest = "SELECT * FROM users LIMIT 1";
-		
 		checkTable(queryTest, queryCreate);
-	};
-	
+    };
+
 	const categoryTableExist = () => {
 		const queryCreate = "CREATE TABLE categories (id INT(100) NOT NULL AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255) NOT NULL, image VARCHAR(255) NOT NULL) ENGINE=InnoDB  DEFAULT CHARSET=utf8";
 		const queryTest = "SELECT * FROM categories LIMIT 1";
-		
 		checkTable(queryTest, queryCreate);
 	};
-	
+
 	const vendorTableExist = () => {
-		const queryCreate = "CREATE TABLE users (id INT(100) NOT NULL AUTO_INCREMENT PRIMARY KEY, uID VARCHAR(100) NOT NULL, username VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, telcode VARCHAR(255) NOT NULL, phone VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, address VARCHAR(255) NOT NULL, profile_picture VARCHAR(255) NOT NULL, verification_status BOOLEAN) ENGINE=InnoDB  DEFAULT CHARSET=utf8";
-		const queryTest = "SELECT * FROM users LIMIT 1";
-		
-		// checkTable(queryTest, queryCreate);
-	};
-	
+		const queryCreate = "CREATE TABLE vendors (id INT(100) NOT NULL AUTO_INCREMENT PRIMARY KEY, sellerID VARCHAR(100) NOT NULL, vendorName VARCHAR(255) NOT NULL) ENGINE=InnoDB  DEFAULT CHARSET=utf8";
+		const queryTest = "SELECT * FROM vendors LIMIT 1";
+		checkTable(queryTest, queryCreate);
+    };
+
     return {
         log,
         ePass,
