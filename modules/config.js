a91fe7298374bdb961b43a906adc7447d793412e
Temiloluwa <tehmi2000@gmail.com>
2019-12-18 05:52:50 +0100

    A new update to the pages

--- modules/config.js
+++ modules/config.js
@@ -31,15 +31,15 @@ const model = function(){
     // const queryTest = "SELECT * FROM users LIMIT 1";
 
     // LOCALHOST CONNECTION
-    const conn = mysql.createConnection({
-        host: 'localhost',
-    	port: 3306,
-        user: 'root',
-   	    password: '',
-        database: "ecomm_db"
-    });
+    // const conn = mysql.createConnection({
+    //     host: 'localhost',
+    // 	port: 3306,
+    //     user: 'root',
+   	//     password: '',
+    //     database: "ecomm_db"
+    // });
     
-    const MONGO_URL = "mongodb://localhost:27017";
+    // const MONGO_URL = "mongodb://localhost:27017";
 
     // AZURE MYSQL CONNECTION
     // const conn = mysql.createConnection({
@@ -51,26 +51,16 @@ const model = function(){
     // });
     
     // JAWDB MYSQL CONNECTION
-<<<<<<< HEAD
-    // const conn = mysql.createConnection({
-    // 	host: "l9dwvv6j64hlhpul.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    // 	port: 3306,
-    // 	user: process.env.MYSQL_USER,
-    // 	password: process.env.MYSQL_PASS,
-    // 	database: "mvqa2ejzg5zltrc0"
-    // });
-=======
-    /*const conn = mysql.createConnection({
+    const conn = mysql.createConnection({
     	host: "l9dwvv6j64hlhpul.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     	port: 3306,
     	user: process.env.MYSQL_USER,
     	password: process.env.MYSQL_PASS,
     	database: "mvqa2ejzg5zltrc0"
-    });*/
->>>>>>> 2a5510e416b97014fc414221520ec97a3ddfde63
+    });
 
     // ATLAS MONGODB CONNECTION
-    // const MONGO_URL = process.env.MONGO_CONNECTION_STRING;
+    const MONGO_URL = process.env.MONGO_CONNECTION_STRING;
 
     const mOptions = {
         useNewUrlParser: true,
