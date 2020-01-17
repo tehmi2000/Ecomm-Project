2fdcf2ebf9620bb4945b856cd969d4f8f6393066
Temiloluwa <tehmi2000@gmail.com>
2020-01-17 10:58:28 +0100

    Vendor page and control page almost complete

--- public/js/vendorApplication.js
+++ public/js/vendorApplication.js
@@ -1,7 +1,59 @@
 let paymentPortalLoaded = false;
+let flagCountry = false;
+
+const createOption = function(container, displayText, value) {
+    let option = createComponent("OPTION", displayText);
+    option.setAttribute("value", value);
+    container.appendChild(option);
+};
+
+const removeOption = function(option) {
+    option.parentNode.removeChild(option);
+};
+
+const getCountries = function(evt) {
+    if(flagCountry === false){
+        fetch(`/api/countries/all`).then(async function(response) {
+            try {
+                let country_list = await response.json();
+                forEach(country_list, function(element) {
+                    createOption(document.querySelector("[name='user-country']"), element.name, element.name);
+                });
+                flagCountry = true;
+    
+            } catch (err) {
+                console.error(err);
+            }
+        }).catch(function(error) {
+            console.error(error);
+        });
+    }
+};
+
+const getRegion = function(evt) {
+    fetch(`/api/countries/all/${evt.currentTarget.value}/getRegions`).then(async function(response) {
+        try {
+            let regionList = await response.json();
+            forEach(document.querySelectorAll("[name='user-region'] > option:not(:first-child)"), function(element) {
+                removeOption(element);
+            });
+
+            if(regionList.length > 0){
+                forEach(regionList, function(element) {
+                    createOption(document.querySelector("[name='user-region']"), element.name, element.name);
+                });
+            }
+
+        } catch (err) {
+            console.error(err);
+        }
+    }).catch(function(error) {
+        console.error(error);
+    });
+};
+
 const addHandlers = function() {
     const form = document.querySelector("#vendor-form");
-    const cardPurse = document.querySelector("#vendor-form .card-purse");
     const container = document.querySelector("#vendor-form .container");
 
     const navOne = document.querySelector(`.form-navigation span:nth-child(1)`);
@@ -69,6 +121,8 @@ const addHandlers = function() {
     nextBtnTwo.addEventListener("click", stepThree);
 
     form.addEventListener("submit", formHandler);
+    document.querySelector("[name='user-country']").addEventListener("click", getCountries);
+    document.querySelector("[name='user-country']").addEventListener("change", getRegion);
 };
 
 document.addEventListener("DOMContentLoaded", function(){
@@ -81,5 +135,32 @@ const submitForm = function(formName){
 
 const formHandler = (evt) => {
     evt.preventDefault();
-    console.log("Form submitted");
+
+    const bodyValue = function() {
+        const retValue = {};
+        const arrayOfElements = document.querySelectorAll("#vendor-form [name]");
+        arrayOfElements.forEach(element => {
+            retValue[`${element.name}`] = element.value;
+        });
+        return retValue;
+    }();
+
+    console.log(bodyValue);
+
+    fetch(``, {
+        method: "POST",
+        body: JSON.stringify(bodyValue),
+        headers: {
+            "Content-Type": "application/json; charset=utf-8"
+        }
+    }).then(async function(response) {
+        try {
+            let result = await response.json();
+            
+        } catch (error) {
+            console.error(error);
+        }
+    }).catch(function(error) {
+        console.error(error);
+    });
 };
