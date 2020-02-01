0585f6ac6f5c8917d42f39c76443941bbc7d35db
Temiloluwa <tehmi2000@gmail.com>
2020-02-01 01:58:27 +0100

    Updated the Vendor Table Create SQL command

--- modules/emailHandler.js
+++ modules/emailHandler.js
@@ -91,6 +91,46 @@ const model = function(){
 		});
 	};
 
+	const vendorReport = function (receiver, fi) {
+		// d-7385c1c6b75e4727adfd2d91a9556900
+		const options = {
+			method: 'POST',
+			url: 'https://api.sendgrid.com/v3/mail/send',
+			headers: {
+				'content-type': 'application/json',
+				authorization: `Bearer ${process.env.SENDGRID_API_KEY}`
+			},
+			body: { 
+				personalizations: [{
+					to: [
+							{
+								email: receiver,
+								name: `${firstname} ${lastname}`
+							}
+					],
+					dynamic_template_data: {
+						last_name: lastname,
+						first_name: firstname,
+						Sender_Name: "Univers",
+						Sender_Address: "No. 14 Wale Erinle Str.",
+						Sender_City: "Itoki-Ota Behind Gas Company",
+						Sender_State: "Ogun State,  Nigeria"
+					},
+					subject: 'Univers Support: Request Acknowledgement'
+				}],
+				from: { email: `${config.ePass['user']}`, name: 'Univers Team' },
+				reply_to: { email: `${config.ePass['user']}`, name: 'Univers Team' },
+				template_id: 'd-29269701ba134385966ee25efab63fae'
+			},
+			json: true
+		};
+
+		request(options, function (error, response, body) {
+			if (error) config.log(error);
+			console.log({statusCode: response.statusCode});
+		});
+	};
+
 	const supportAcknowledge = async function(receiver, firstname, lastname){
 		const options = {
 			method: 'POST',
@@ -134,7 +174,8 @@ const model = function(){
 		sendVerificationMail: validation,
 		sendNotificationMail: notification,
 		sendPasswordReset: resetter,
-		sendAcknowledgeSupport: supportAcknowledge
+		sendAcknowledgeSupport: supportAcknowledge,
+		sendVendorStatusReport: vendorReport
 	};
 };
 
